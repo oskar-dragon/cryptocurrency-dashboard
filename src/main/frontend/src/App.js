bfff79125180a104ec1a69b6c33478e1fefd7086
Oskar Dragon <oskartdragon@gmail.com>
2022-01-03 16:57:05 +0000

    React Router Setup

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+import { CoinDetails } from "./pages";
+import Home from "./pages/Home";
+import * as ROUTES from "./constants/routes";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <Routes>
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.COIN_DETAILS} element={<CoinDetails />} />
+    </Routes>
   );
 }
 
---
+++ src/main/frontend/src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as Home } from "./Home";
+export { default as CoinDetails } from "./CoinDetails";
