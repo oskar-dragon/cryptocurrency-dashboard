175490b5090871a4f4a408d50251201d2d4820a2
bmwrabel <boris.wrabel@googlemail.com>
2022-01-03 17:17:21 +0000

    refactoring

--- src/main/java/com/dragcorp/cryptodashboard/service/NewsService.java
+++ src/main/java/com/dragcorp/cryptodashboard/service/NewsService.java
@@ -1,15 +1,18 @@
 package com.dragcorp.cryptodashboard.service;
 
 import com.dragcorp.cryptodashboard.client.CryptoCompareClient;
+import com.dragcorp.cryptodashboard.client.response.cryptocompare.ArticleResponse;
 import com.dragcorp.cryptodashboard.client.response.cryptocompare.NewsResponse;
+import com.dragcorp.cryptodashboard.model.News;
 import com.dragcorp.cryptodashboard.repository.NewsRepository;
 import com.dragcorp.cryptodashboard.model.Article;
 import com.dragcorp.cryptodashboard.model.ArticleSourceInfo;
-import com.dragcorp.cryptodashboard.model.News;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NewsService {
@@ -28,35 +31,34 @@ public class NewsService {
     return newsRepository.findFirstByOrderByCreatedAtDesc(LocalDate.now());
   }
 
-  public NewsResponse getNewsFromApi() {
+  public void fetchNewsFromApi() {
     NewsResponse news = cryptoCompareClient.getNews();
-//    List<Article> convertedNews = news.getData()
-//        .stream()
-//        .map(this::convertToArticle)
-//        .collect(Collectors.toList());
-//    newsRepository.insert(new NewsData(LocalDate.now(), convertedNews));
-    return news;
+    List<Article> convertedNews = news.getData()
+        .stream()
+        .map(this::convertToArticle)
+        .collect(Collectors.toList());
+    newsRepository.insert(new News(LocalDate.now(), convertedNews));
   }
 
-  private Article convertToArticle(com.dragcorp.cryptodashboard.client.response.cryptocompare.News news) {
+  private Article convertToArticle(ArticleResponse article) {
     return new Article(
-        news.getId(),
-        news.getGuid(),
-        news.getPublishedOn(),
-        news.getImageUrl(),
-        news.getTitle(),
-        news.getUrl(),
-        news.getSource(),
-        news.getBody(),
-        news.getTags(),
-        news.getCategories(),
-        news.getUpvotes(),
-        news.getDownvotes(),
-        news.getLang(),
+        article.getId(),
+        article.getGuid(),
+        article.getPublishedOn(),
+        article.getImageUrl(),
+        article.getTitle(),
+        article.getUrl(),
+        article.getSource(),
+        article.getBody(),
+        article.getTags(),
+        article.getCategories(),
+        article.getUpvotes(),
+        article.getDownvotes(),
+        article.getLang(),
         new ArticleSourceInfo(
-            news.getSourceInfo().getName(),
-            news.getSourceInfo().getLang(),
-            news.getSourceInfo().getImg()
+            article.getSourceInfo().getName(),
+            article.getSourceInfo().getLang(),
+            article.getSourceInfo().getImg()
         )
     );
   }
