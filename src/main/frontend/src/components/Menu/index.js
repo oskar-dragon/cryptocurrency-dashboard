fbfb78163049d1037a61b40210ef12afcb3700ad
Oskar Dragon <oskartdragon@gmail.com>
2022-01-05 20:12:34 +0000

    Fixed props

--- src/main/frontend/src/components/Menu/index.js
+++ src/main/frontend/src/components/Menu/index.js
@@ -1,22 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Logo, Group, Link } from "./style/menu.styled";
 
-export default function Menu({ children, restProps }) {
+export default function Menu({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
-Menu.Logo = function MenuLogo({ children, restProps }) {
+Menu.Logo = function MenuLogo({ children, ...restProps }) {
   return <Logo {...restProps}>{children}</Logo>;
 };
 
-Menu.Group = function MenuGroup({ children, restProps }) {
+Menu.Group = function MenuGroup({ children, ...restProps }) {
   return <Group {...restProps}>{children}</Group>;
 };
 
-Menu.Link = function MenuLink({ to, children, restProps }) {
-  return (
-    <Link to={to} {...restProps}>
-      {children}
-    </Link>
-  );
+Menu.Link = function MenuLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
 };
