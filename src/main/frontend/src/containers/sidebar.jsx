40d947cd8f178130f3700937dc55711c237ebe5b
Oskar Dragon <oskartdragon@gmail.com>
2022-01-05 20:13:10 +0000

    Add state and handling function

--- src/main/frontend/src/containers/sidebar.jsx
+++ src/main/frontend/src/containers/sidebar.jsx
@@ -3,17 +3,39 @@ import { Menu } from "../components";
 import * as ROUTES from "../constants/routes";
 
 export default function Sidebar() {
-  //TODO: Add state for checking which one is active and which one isn't.
+  const [currentLink, setCurrentLink] = useState(ROUTES.HOME);
+
+  function handleClick(route) {
+    setCurrentLink(route);
+  }
 
   return (
     <Menu>
       <Menu.Logo>Coin Meerkat</Menu.Logo>
       <Menu.Group>
-        <Menu.Link to={ROUTES.HOME}>Dashboard</Menu.Link>
-        <Menu.Link to={ROUTES.HOME}>Exchanges</Menu.Link>
-        <Menu.Link to={ROUTES.HOME}>Portfolio</Menu.Link>
-        <Menu.Link to={ROUTES.HOME}>Watchlist</Menu.Link>
-        <Menu.Link to={ROUTES.HOME}>Settings</Menu.Link>
+        <Menu.Link
+          onClick={() => handleClick(ROUTES.HOME)}
+          active={currentLink === ROUTES.HOME}
+          to={ROUTES.HOME}
+        >
+          Dashboard
+        </Menu.Link>
+        <Menu.Link
+          onClick={() => handleClick(ROUTES.COIN_DETAILS)}
+          active={currentLink === ROUTES.COIN_DETAILS}
+          to={ROUTES.HOME}
+        >
+          Exchanges
+        </Menu.Link>
+        <Menu.Link active={currentLink === ROUTES.HOME} to={ROUTES.HOME}>
+          Portfolio
+        </Menu.Link>
+        <Menu.Link active={currentLink === ROUTES.HOME} to={ROUTES.HOME}>
+          Watchlist
+        </Menu.Link>
+        <Menu.Link active={currentLink === ROUTES.HOME} to={ROUTES.HOME}>
+          Settings
+        </Menu.Link>
       </Menu.Group>
     </Menu>
   );
