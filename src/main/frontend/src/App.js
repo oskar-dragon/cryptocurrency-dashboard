b6bd9430f126a26bfb9c1377320ee020fbefd1ca
Oskar Dragon <oskartdragon@gmail.com>
2022-01-08 11:27:25 +0000

    Add page wrapper component

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Home, CoinDetails, NotFound } from "./Pages";
 import { Sidebar } from "./Containers";
+import { PageWrapper } from "./Components";
 import * as ROUTES from "./Constants/routes";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Sidebar /> */}
-      <Routes>
-        <Route path={ROUTES.HOME} element={<Home />} />
-        <Route path={ROUTES.COIN_DETAILS} element={<CoinDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <PageWrapper>
+        <PageWrapper.Aside>
+          <Sidebar />
+        </PageWrapper.Aside>
+        <PageWrapper.Main>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.COIN_DETAILS} element={<CoinDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </PageWrapper.Main>
+      </PageWrapper>
     </>
   );
 }
