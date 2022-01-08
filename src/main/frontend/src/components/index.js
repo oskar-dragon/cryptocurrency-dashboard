0b2e3a3009132b40813fd87c649c8363bcaee6fe
Oskar Dragon <oskartdragon@gmail.com>
2022-01-08 11:28:10 +0000

    export PageWrapper and Segment

--- src/main/frontend/src/components/index.js
+++ src/main/frontend/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Menu } from "./Menu";
 export { default as Card } from "./Card";
+export { default as PageWrapper } from "./PageWrapper";
+export { default as Segment } from "./Segment";
