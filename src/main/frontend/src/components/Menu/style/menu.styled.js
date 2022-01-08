b602e5bc5a0b9b109bdb3eb66d79601361c32a0a
Oskar Dragon <oskartdragon@gmail.com>
2022-01-08 11:28:29 +0000

    Removed width and position

--- src/main/frontend/src/components/Menu/style/menu.styled.js
+++ src/main/frontend/src/components/Menu/style/menu.styled.js
@@ -6,9 +6,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   padding: 1.5em;
-  width: 250px;
   height: 100%;
-  position: fixed;
 `;
 
 export const Logo = styled.p`
